d1802897bc8f27ffa77ffc5e5130efa3e918c6c7
Ciaran Whitty <whittyciaran1998@gmail.com>
2019-03-10 17:11:39 +0000

    mainly xml work

--- app/src/main/java/org/wit/formula_1/activities/GameListActivity.kt
+++ app/src/main/java/org/wit/formula_1/activities/GameListActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.activity_game_list.*
@@ -21,9 +22,21 @@ lateinit var app: MainApp
 
 override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
     setContentView(R.layout.activity_game_list)
+
     app = application as MainApp
 
+    toolbarMain.title = title
+    setSupportActionBar(toolbarMain)
+
+
 }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
 }
 
