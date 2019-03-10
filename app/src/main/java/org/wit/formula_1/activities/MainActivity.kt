0710e11cd70716e6d8127b579b120019266c6031
cwhitty <whittyciaran1998@gmail.com>
2019-03-10 20:36:59 +0000

    switching screens

--- app/src/main/java/org/wit/formula_1/activities/MainActivity.kt
+++ app/src/main/java/org/wit/formula_1/activities/MainActivity.kt
@@ -2,6 +2,9 @@ package org.wit.formula_1.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_game_list.*
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
@@ -13,12 +16,16 @@ import org.wit.formula_1.models.GameModel
 class MainActivity : AppCompatActivity(), AnkoLogger {
 
     var game = GameModel()
-    lateinit var app : MainApp
+    lateinit var app: MainApp
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         app = application as MainApp
+
+        toolbarAdd.title = title
+        setSupportActionBar(toolbarAdd)
 
         info("MainActivity has started")
 
@@ -30,14 +37,34 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
             if (game.title.isNotEmpty()) {
                 app.games.add(game.copy())
                 info("add Button Pressed: $game")
-                app.games.forEach { info("add Button Pressed: ${it}")}
-            }
-            else {
-                toast ("Please Enter a title")
+                app.games.forEach { info("add Button Pressed: ${it}") }
+            } else {
+                toast("Please Enter a title")
             }
         }
 
 
 
     }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_cancel, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override  fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.item_cancel -> {
+                finish()
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+
+
+
 }
