409b9985d240b077fcc970c485cd0bb8a3f2f104
Ciaran Whitty <whittyciaran1998@gmail.com>
2019-04-30 22:15:28 +0100

    Games - json

--- app/src/main/java/org/wit/formula_1/main/MainApp.kt
+++ app/src/main/java/org/wit/formula_1/main/MainApp.kt
@@ -3,20 +3,19 @@ package org.wit.formula_1.main
 import android.app.Application
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
-import org.wit.formula_1.models.GameMemStore
-import org.wit.formula_1.models.PlayerMemStore
+import org.wit.formula_1.models.*
 
 class MainApp : Application(), AnkoLogger {
 
-    //val games = ArrayList<GameModel>()
-    val games = GameMemStore()
-
-    //val players = ArrayList<PlayerModel>()
-    val players = PlayerMemStore()
+    lateinit var games: GameStore
+    lateinit var players: PlayerStore
 
     override fun onCreate() {
         super.onCreate()
 
+        games = GameJSONStore(applicationContext)
+
+        players = PlayerMemStore()
 
         info("" + "Game started")
     }
---
+++ app/src/main/java/org/wit/formula_1/models/GameJsonStore.kt
@@ -0,0 +1,61 @@
+package org.wit.formula_1.models
+
+import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import org.jetbrains.anko.AnkoLogger
+import org.wit.formula_1.helpers.*
+import java.util.*
+
+val JSON_FILE = "games.json"
+val gsonBuilder = GsonBuilder().setPrettyPrinting().create()
+val listType = object : TypeToken<java.util.ArrayList<GameModel>>() {}.type
+
+fun generateRandomId(): Long {
+    return Random().nextLong()
+}
+
+class GameJSONStore : GameStore, AnkoLogger {
+
+    val context: Context
+    var games = mutableListOf<GameModel>()
+
+    constructor (context: Context) {
+        this.context = context
+        if (exists(context, JSON_FILE)) {
+            deserialize()
+        }
+    }
+
+    override fun findAll(): MutableList<GameModel> {
+        return games
+    }
+
+    override fun create(game: GameModel) {
+        game.gid = generateRandomId()
+        games.add(game)
+        serialize()
+    }
+
+
+    override fun update(game: GameModel) {
+        val gamesList = findAll() as ArrayList<GameModel>
+        var foundGame: GameModel? = gamesList.find { p -> p.gid == game.gid }
+        if (foundGame != null) {
+            foundGame.title = game.title
+            foundGame.description = game.description
+        }
+        serialize()
+    }
+
+    private fun serialize() {
+        val jsonString = gsonBuilder.toJson(games, listType)
+        write(context, JSON_FILE, jsonString)
+    }
+
+    private fun deserialize() {
+        val jsonString = read(context, JSON_FILE)
+        games = Gson().fromJson(jsonString, listType)
+    }
+}
